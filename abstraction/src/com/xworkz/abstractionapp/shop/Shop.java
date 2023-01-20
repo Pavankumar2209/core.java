416bb363909b0e72b3d5ef7e2d64e492aebbf24a
Pavankumar2209 <pavand.xworkz@gmail.com>
2023-01-21 00:51:17 +0530

    Opps concepts

---
+++ abstraction/src/com/xworkz/abstractionapp/shop/Shop.java
@@ -0,0 +1,13 @@
+package com.xworkz.abstractionapp.shop;
+
+
+//interface created 
+public interface Shop {
+	
+	
+	//abstract method: a method does not contain body that method is called abstract method.
+	public long doBusiness();
+	
+	
+
+}
